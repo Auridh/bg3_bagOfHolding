1bc74fd788fc0d79c57b5b2be0f27ec767522c5a
Auridh <git@auridh.net>
2023-08-25 00:11:56 +0200

    :sparkles: First working version

--- mod/Mods/Auridh_BoH/ScriptExtender/Lua/Server/BagOfHolding.lua
+++ mod/Mods/Auridh_BoH/ScriptExtender/Lua/Server/BagOfHolding.lua
@@ -1,77 +1,82 @@
--- Persistent data stored even when the game is quit
-PersistentVars = {}
-
--- Table indices
-local Idx_InitialItemsGiven = 'InitialItemsGiven'
-
 -- Item uuids
 local Uid_BagOfHolding = '7471c9a3-e826-4ff2-b04c-b47c5360e46f'
 
--- Debug Messages
-local Msg_Debug_SavegameLoaded = 'BoH: Savegame loaded!'
-local Msg_Debug_SessionLoaded = 'BoH: Session loaded!'
-local Msg_Debug_ItemsGiven = 'Boh: Items given'
-local Msg_Debug_ItemsNotGiven = 'BoH: Items already given'
+-- Status names
+local Status_ReduceWeightChar = 'AC_REDUCE_WEIGHT_CHAR'
+
+-- Self defined events
+local Event_BagSearch = 'BoH_BagSearch'
+local Event_BagSearchEnd = 'BoH_BagSearchEnd'
+local Event_BagSearchSub = 'BoH_BagSearchSub'
+local Event_BagSearchSubEnd = 'BoH_BagSearchSubEnd'
+local Event_ReduceWeight = 'BoH_ReduceWeight'
+local Event_ReduceWeightEnd = 'BoH_ReduceWeightEnd'
 
 -- Osiris Events
-local Osi_Evt_SavegameLoaded = 'SavegameLoaded'
-
-function Tests()
-	-- Print items in the inventory to console
-	--Osi.IterateInventory(Osi.GetHostCharacter(), "DEBUG_CONSOLE_IterateInventory", "DEBUG_CONSOLE_IterateInventoryDone")
-	Osi.IterateInventoryByTag(Osi.GetHostCharacter(), "BoH_BAG", "DEBUG_CONSOLE_IterateInventory", "DEBUG_CONSOLE_IterateInventoryDone")
-end
-
-local foundItems = {}
-Ext.Osiris.RegisterListener("EntityEvent", 2, "after", function (entity, event)
-	if event == "DEBUG_CONSOLE_IterateInventory" then
-		foundItems[#foundItems+1] = entity
-	elseif event == "DEBUG_CONSOLE_IterateInventoryDone" then
-		Ext.Utils.Print("Iteration complete!")
-		Ext.Utils.PrintWarning("=====")
-		Ext.Dump(foundItems)
-		foundItems = {}
-		Ext.Utils.PrintWarning("=====")
-	end
-end)
+local Osi_Evt_EntityEvent = 'EntityEvent'
 
 
-function CheckInitialItemsForCharacter(character)
-	local charIdx = Idx_InitialItemsGiven .. '_' .. character
 
-	if not PersistentVars[charIdx] then
-		Osi.TemplateAddTo(Uid_BagOfHolding, character, 1, 1)
-		PersistentVars[charIdx] = true
-		Ext.Utils.Print(Msg_Debug_ItemsGiven)
-	else
-		Ext.Utils.Print(Msg_Debug_ItemsNotGiven)
-	end
-end
-
-function CheckInitialItems()
+function ExecuteForPlayerCharacters(action)
 	-- Get party members from db
 	local partyMembers = Osi.DB_PartyMembers:Get(nil)
 
 	for _, v in pairs(partyMembers) do
-		CheckInitialItemsForCharacter(v[1])
+		action(v[1])
 	end
 end
 
-function OnSessionLoaded()
-	Ext.Utils.Print(Msg_Debug_SessionLoaded)
-	Ext.Utils.Print(PersistentVars['Test'])
-	PersistentVars['Test'] = 'Something to keep'
-	Ext.Utils.Print(Ext.Json.Stringify(PersistentVars))
+function ApplyReducedWeightToItem(entity)
+	Osi.RemoveStatus(entity, Status_ReduceWeightChar, entity)
+	Osi.ApplyStatus(entity, Status_ReduceWeightChar, 60, 1, entity)
 end
 
-function OnSavegameLoaded()
-    Ext.Utils.Print(Msg_Debug_SavegameLoaded)
-	CheckInitialItems()
-	Tests()
+function ApplyReducedWeightByPlayer(player)
+	Osi.IterateInventoryByTemplate(player, Uid_BagOfHolding, Event_BagSearch, Event_BagSearchEnd)
+end
+
+local tickCounter = 0
+function OnTick()
+	if tickCounter < 1000 then
+		tickCounter = tickCounter + 1
+	else
+		tickCounter = 0
+		ExecuteForPlayerCharacters(ApplyReducedWeightByPlayer)
+	end
 end
 
 -- Event listeners
-Ext.Events.SessionLoaded:Subscribe(OnSessionLoaded)
+Ext.Events.Tick:Subscribe(OnTick)
 
 -- Osiris event listeners
-Ext.Osiris.RegisterListener(Osi_Evt_SavegameLoaded, 0, "after", OnSavegameLoaded)
+Ext.Osiris.RegisterListener(Osi_Evt_EntityEvent, 2, 'after', function (entity, event)
+	local choices = {
+		[Event_BagSearch] = function(item)
+			--Ext.Utils.Print(Event_BagSearch .. ' | ' .. item)
+			Osi.IterateInventory(
+					item,
+					Event_ReduceWeight,
+					Event_ReduceWeightEnd)
+		end,
+		[Event_BagSearchSub] = function(item)
+			--Ext.Utils.Print(Event_BagSearchSub .. ' | ' .. item)
+			ApplyReducedWeightToItem(item)
+		end,
+		[Event_ReduceWeight] = function(item)
+			--Ext.Utils.Print(Event_ReduceWeight .. ' | ' .. item)
+			if Osi.IsContainer(item) == 1 then
+				Osi.IterateInventory(
+						item,
+						Event_BagSearchSub,
+						Event_BagSearchSubEnd)
+			else
+				ApplyReducedWeightToItem(item)
+			end
+		end
+	}
+
+	local func = choices[event]
+	if func then
+		func(entity)
+	end
+end)
