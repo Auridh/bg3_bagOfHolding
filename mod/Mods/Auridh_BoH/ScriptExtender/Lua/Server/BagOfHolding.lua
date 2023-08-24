9322084ba3cd763fe47951301d91bab84f1126a1
Auridh <git@auridh.net>
2023-08-24 03:10:33 +0200

    :construction: Add debug logs to script

--- mod/Mods/Auridh_BoH/ScriptExtender/Lua/Server/BagOfHolding.lua
+++ mod/Mods/Auridh_BoH/ScriptExtender/Lua/Server/BagOfHolding.lua
@@ -1,12 +1,34 @@
+-- Persistent data stored even when the game is quit
 PersistentVars = {}
 
-function InitialItemsGiven()
-	PersistentVars['InitialItemsGiven'] = true
+-- Table indices
+Idx_InitialItemsGiven = 'InitialItemsGiven'
+
+-- Item uuids
+Uid_BagOfHolding = '7471c9a3-e826-4ff2-b04c-b47c5360e46f'
+
+-- Debug Texts
+Msg_Debug_SavegameLoaded = 'BoH: Savegame loaded!'
+Msg_Debug_ItemsGiven = 'Boh: Items given'
+Msg_Debug_ItemsNotGiven = 'BoH: Items already given'
+
+-- Osiris Events
+Evt_SavegameLoaded = 'SavegameLoaded'
+
+
+function CheckInitialItems()
+	if not PersistentVars[Idx_InitialItemsGiven] then
+		Osi.TemplateAddTo(Uid_BagOfHolding, GetHostCharacter(), 1, 1)
+		PersistentVars[Idx_InitialItemsGiven] = true
+		Ext.Utils.Print(Msg_Debug_ItemsGiven)
+	else
+		Ext.Utils.Print(Msg_Debug_ItemsNotGiven)
+	end
 end
 
 function OnSavegameLoaded()
-    Ext.Utils.Print("BoH: Savegame loaded!")
-	Osi.TemplateAddTo("7471c9a3-e826-4ff2-b04c-b47c5360e46f", GetHostCharacter(), 1, 1)
+    Ext.Utils.Print(Msg_Debug_SavegameLoaded)
+	CheckInitialItems()
 end
 
-Ext.Osiris.RegisterListener("SavegameLoaded", 0, "after", OnSavegameLoaded)
+Ext.Osiris.RegisterListener(Evt_SavegameLoaded, 0, "after", OnSavegameLoaded)
