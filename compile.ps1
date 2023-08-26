2403e20326fec5253764cf52158b79885f2e7b3d
Auridh <git@auridh.net>
2023-08-26 03:10:34 +0200

    :sparkles: Add French and German localization

--- compile.ps1
+++ compile.ps1
@@ -1,5 +1,7 @@
 $absPath = Resolve-Path -Path "./"
 divine -a convert-loca -g bg3 -s "$absPath/mod/Localization/English/BagOfHolding.xml" -d "$absPath/mod/Localization/English/BagOfHolding.loca";
+divine -a convert-loca -g bg3 -s "$absPath/mod/Localization/French/BagOfHolding.xml" -d "$absPath/mod/Localization/French/BagOfHolding.loca";
+divine -a convert-loca -g bg3 -s "$absPath/mod/Localization/German/BagOfHolding.xml" -d "$absPath/mod/Localization/German/BagOfHolding.loca";
 divine -a convert-resources -g bg3 -s "$absPath/mod/Public/Auridh_BoH/Content/UI/[PAK]_UI" -d "$absPath/mod/Public/Auridh_BoH/Content/UI/[PAK]_UI" -i lsx -o lsf;
 divine -a convert-resources -g bg3 -s "$absPath/mod/Public/Auridh_BoH/GUI" -d "$absPath/mod/Public/Auridh_BoH/GUI" -i lsx -o lsf;
 divine -a convert-resources -g bg3 -s "$absPath/mod/Public/Auridh_BoH/RootTemplates" -d "$absPath/mod/Public/Auridh_BoH/RootTemplates" -i lsx -o lsf;
--- compile_tut.ps1
+++ compile_tut.ps1
@@ -4,6 +4,8 @@ Move-Item -Path "$absPath/mod/Public/Auridh_BoH/Stats/Generated/TreasureTable.tx
 Copy-Item -Path "$absPath/TuTSumTable.txt" -Destination "$absPath/mod/Public/Auridh_BoH/Stats/Generated/TreasureTable.txt" -Force
 
 divine -a convert-loca -g bg3 -s "$absPath/mod/Localization/English/BagOfHolding.xml" -d "$absPath/mod/Localization/English/BagOfHolding.loca";
+divine -a convert-loca -g bg3 -s "$absPath/mod/Localization/French/BagOfHolding.xml" -d "$absPath/mod/Localization/French/BagOfHolding.loca";
+divine -a convert-loca -g bg3 -s "$absPath/mod/Localization/German/BagOfHolding.xml" -d "$absPath/mod/Localization/German/BagOfHolding.loca";
 divine -a convert-resources -g bg3 -s "$absPath/mod/Public/Auridh_BoH/Content/UI/[PAK]_UI" -d "$absPath/mod/Public/Auridh_BoH/Content/UI/[PAK]_UI" -i lsx -o lsf;
 divine -a convert-resources -g bg3 -s "$absPath/mod/Public/Auridh_BoH/GUI" -d "$absPath/mod/Public/Auridh_BoH/GUI" -i lsx -o lsf;
 divine -a convert-resources -g bg3 -s "$absPath/mod/Public/Auridh_BoH/RootTemplates" -d "$absPath/mod/Public/Auridh_BoH/RootTemplates" -i lsx -o lsf;
