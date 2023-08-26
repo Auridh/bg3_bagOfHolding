3206a09a0bfc35253ae7782ac1856d74639c75e8
Auridh <git@auridh.net>
2023-08-26 18:17:45 +0200

    :coffin: Remove unused functions

--- mod/Mods/Auridh_BoH/ScriptExtender/Lua/Server/BagOfHolding.lua
+++ mod/Mods/Auridh_BoH/ScriptExtender/Lua/Server/BagOfHolding.lua
@@ -1,53 +1,27 @@
 -- Item uuids
-local Uid_BagOfHolding = '7471c9a3-e826-4ff2-b04c-b47c5360e46f'
+local Name_BagOfHolding = 'BoH_OBJ_BagOfHolding'
 
 -- Status names
 local Status_ReduceWeightChar = 'AC_REDUCE_WEIGHT_CHAR'
-
--- Self defined events
-local Event_BagSearch = 'BoH_BagSearch'
-local Event_BagSearchEnd = 'BoH_BagSearchEnd'
 
 -- Osiris Events
 local Osi_Evt_ItemMoved = 'Moved'
 local Osi_Evt_AddedTo = 'AddedTo'
 
 
-function ExecuteForPlayerCharacters(action)
-	-- Get party members from db
-	local partyMembers = Osi.DB_PartyMembers:Get(nil)
-
-	for _, v in pairs(partyMembers) do
-		action(v[1])
-	end
-end
-
-function ApplyReducedWeightToItem(entity)
-	if Osi.HasActiveStatus(entity, Status_ReduceWeightChar) ~= 1 then
-		Osi.ApplyStatus(entity, Status_ReduceWeightChar, -1, 1, entity)
-	end
-end
-
-function ApplyReducedWeightByPlayer(player)
-	Osi.IterateInventoryByTemplate(player, Uid_BagOfHolding, Event_BagSearch, Event_BagSearchEnd)
-end
-
 -- Osiris event listeners
 Ext.Osiris.RegisterListener(Osi_Evt_ItemMoved, 1, 'after', function(entity)
 	if Osi.HasActiveStatus(entity, Status_ReduceWeightChar) == 1 then
-		--Ext.Utils.Print('MOVED!! -> ' .. entity)
 		Osi.RemoveStatus(entity, Status_ReduceWeightChar, entity)
 	end
 end)
 
 Ext.Osiris.RegisterListener(Osi_Evt_AddedTo, 3, 'after', function(entity, char, type)
 	if Osi.HasActiveStatus(entity, Status_ReduceWeightChar) == 1 then
-		--Ext.Utils.Print('AddedTo!! -> ' .. entity .. ' | ' .. char .. ' | ' .. type)
 		Osi.RemoveStatus(entity, Status_ReduceWeightChar, entity)
 	end
 
-	--Ext.Utils.Print(string.sub(char, 1, 20))
-	if Osi.IsInInventory(entity) == 1 and string.sub(char, 1, 20) == 'BoH_OBJ_BagOfHolding' then
+	if Osi.IsInInventory(entity) == 1 and string.sub(char, 1, 20) == Name_BagOfHolding then
 		Osi.ApplyStatus(entity, Status_ReduceWeightChar, -1, 1, entity)
 	end
 end)
