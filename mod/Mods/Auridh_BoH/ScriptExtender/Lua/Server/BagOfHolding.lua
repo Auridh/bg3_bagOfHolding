6616b21a009f3474189edf46b9be660aa55a4c91
Auridh <git@auridh.net>
2023-08-29 02:14:43 +0200

    :bug: Fix display of currently carried weight

--- mod/Mods/Auridh_BoH/ScriptExtender/Lua/Server/BagOfHolding.lua
+++ mod/Mods/Auridh_BoH/ScriptExtender/Lua/Server/BagOfHolding.lua
@@ -3,6 +3,7 @@ local Name_BagOfHolding = 'BoH_OBJ_BagOfHolding'
 
 -- Status names
 local Status_ReduceWeightChar = 'AC_REDUCE_WEIGHT_CHAR'
+local Status_WeightDisplayFix = 'BH_REDUCE_WEIGHT_FIX'
 
 -- Osiris Events
 local Osi_Evt_AddedTo = 'AddedTo'
@@ -13,11 +14,13 @@ local Osi_Evt_RemovedFrom = 'RemovedFrom'
 Ext.Osiris.RegisterListener(Osi_Evt_RemovedFrom, 2, 'before', function(entity, container)
 	if Osi.HasActiveStatus(entity, Status_ReduceWeightChar) == 1 and Osi.GetStatString(container) == Name_BagOfHolding then
 		Osi.RemoveStatus(entity, Status_ReduceWeightChar, container)
+		Osi.ApplyStatus(Osi.GetOwner(container), Status_WeightDisplayFix, 0, 1, container)
 	end
 end)
 
 Ext.Osiris.RegisterListener(Osi_Evt_AddedTo, 3, 'before', function(entity, container, type)
 	if Osi.HasActiveStatus(entity, Status_ReduceWeightChar) ~= 1 and Osi.GetStatString(container) == Name_BagOfHolding then
 		Osi.ApplyStatus(entity, Status_ReduceWeightChar, -1, 1, container)
+		Osi.ApplyStatus(Osi.GetOwner(container), Status_WeightDisplayFix, 0, 1, container)
 	end
 end)
